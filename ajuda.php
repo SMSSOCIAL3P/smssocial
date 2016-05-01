3d6b677496caf38b71d512a1db16bdcad97acd48
SMSSOCIAL3P <smssocial2015@gmail.com>
2016-05-01 11:30:56 -0300

    Added files via upload

---
+++ ajuda.php
@@ -0,0 +1,38 @@
+<div class="login-box" style="width:720px;">
+  <div class="login-logo">
+    <div id="blog-title">
+      <span>
+        <!--<b>SMS</b> Social-->
+        <img src="<?php bloginfo('stylesheet_directory'); ?>/imagens/smsSocial.png" width="160" title="SMSSocial">
+      </span>
+    </div>
+  </div><!-- /.login-logo -->
+  <div class="login-box-body">
+    <section class="content-header">
+      <h1>Ajuda</h1>
+    </section>
+    <div class="col-xs-12">&nbsp;</div>
+    <div class="row">
+      <div class="col-xs-6">
+          <div class="box">
+            <div class="box-body">
+              <center>
+              <a style="font-size:45pt" href="https://www.youtube.com/watch?v=QbupTnE_-sg" target="blank"><i class="fa fa-play-circle-o"></i></a><br>
+              <span style="font-size:10pt">Vídeo Tutorial</span>
+              </center>
+            </div>
+          </div>
+      </div>
+      <div class="row">
+      <div class="col-xs-6">
+          <div class="box">
+            <div class="box-body">
+              <center>
+              <a style="font-size:45pt" href="http://smssocial.gi.fic.ufg.br/tutoriais/" taget="blank"><i class="fa fa-book"></i></a><br>
+              <span style="font-size:10pt">Manual do Usuário</span>
+              </center>
+            </div>
+          </div>
+      </div>
+  </div><!-- /.login-box-body -->
+</div><!-- /.fale-box -->
---
+++ fale_conosco.php
@@ -0,0 +1,50 @@
+<div class="login-box" style="width:720px;">
+  <div class="login-logo">
+    <div id="blog-title">
+      <span>
+        <!--<b>SMS</b> Social-->
+        <img src="<?php bloginfo('stylesheet_directory'); ?>/imagens/smsSocial.png" width="160" title="SMSSocial">
+      </span>
+    </div>
+  </div><!-- /.login-logo -->
+  <div class="login-box-body">
+    <section class="content-header">
+      <h1>Fale Conosco</h1>
+    </section>
+    <div class="col-xs-12">&nbsp;</div>
+    <form name="form" id="form" action="<?php bloginfo('template_url'); ?>/login/actions.php?mt=fale_conosco" method="post" >
+      <div class="col-xs-12">
+        <label> Nome </label>
+        <input type="text" name="nome" value="" placeholder="Nome" class="form-control required">
+      </div>
+      <div class="col-xs-12">&nbsp;</div>
+
+      <div class="col-xs-6">
+        <label> Email </label>
+        <input type="text" name="email" value="" placeholder="Email" class="form-control required">
+      </div>
+
+      <div class="col-xs-6">
+        <label> Telefone </label>
+        <input type="text" name="telefone" value="" placeholder="Telefone" class="form-control required">
+      </div>
+      <div class="col-xs-12">&nbsp;</div>
+
+      <div class="col-xs-12">
+        <label> Texto </label>
+        <textarea rows="2" cols="50" name="texto" class="form-control required"></textarea>
+      </div>
+      <div class="col-xs-12">&nbsp;</div>
+      <div class="row">
+        <div class="col-xs-12">
+          <button class="btn  btn-primary" type="submit">Enviar</button>
+        </div>
+      </div>
+    </form>
+  </div><!-- /.login-box-body -->
+</div><!-- /.fale-box -->
+<script>
+$().ready(function() {
+  $('#form').validate({});
+});
+</script>

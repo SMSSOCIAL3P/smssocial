23c478ce650938f1a6b5c09b564c15de8d94fcd4
smssocial3p <smssocial2015@gmail.com>
2016-05-02 06:59:04 -0300

    Commit 3
    commit

---
+++ home/index.php
@@ -0,0 +1,89 @@
+<!-- Main content -->
+<section class="content">
+  	<div class="row">
+    	<div class="col-xs-4">
+	      	<div class="box">
+	        	<div class="box-body">
+	        		<center>
+        			<a href="<?php bloginfo('template_url'); ?>/controller.php?ctr=mensagem&mt=addMensagem"><i class="fa fa-comment-o"></i></a><br>
+        			<span>Nova Mensagem</span>
+        			</center>
+	        	</div>
+	        </div>
+	    </div>
+	    <div class="col-xs-4">
+	      	<div class="box">
+	        	<div class="box-body">
+	        		<center>
+        			<a href="<?php bloginfo('template_url'); ?>/controller.php?ctr=respostaMsg"><i class="fa fa-inbox"></i></a><br>
+        			<span>Mensagens Recebidas</span>
+        			</center>
+	        	</div>
+	        </div>
+	    </div>
+	    <div class="col-xs-4">
+	      	<div class="box">
+	        	<div class="box-body">
+	        		<center>
+        			<a href="<?php bloginfo('template_url'); ?>/controller.php?ctr=pessoas&mt=addPessoas"><i class="fa fa-user"></i></a><br>
+        			<span>Cadastrar Pessoas</span>
+        			</center>
+	        	</div>
+	        </div>
+	    </div>
+	</div>
+  	<div class="row">
+    	<div class="col-xs-4">
+	      	<div class="box">
+	        	<div class="box-body">
+	        		<center>
+        			<a href="https://www.youtube.com/watch?v=QbupTnE_-sg" target="blank"><i class="fa fa-play-circle-o"></i></a><br>
+        			<span>V&iacute;deo</span>
+        			</center>
+	        	</div>
+	        </div>
+	    </div>
+	    <div class="col-xs-4">
+	      	<div class="box">
+	        	<div class="box-body">
+	        		<center>
+        			<a href="http://smssocial.gi.fic.ufg.br/tutoriais/" target="blank"><i class="fa fa-book"></i></a><br>
+        			<span>Manual do Usu&aacute;rio</span>
+        			</center>
+	        	</div>
+	        </div>
+	    </div>
+	    <div class="col-xs-4">
+	      	<div class="box">
+	        	<div class="box-body">
+	        		<center>
+        			<a href="#"><i class="fa fa-dollar"></i></a><br>
+        			<span>Relatório de Cr&eacute;ditos</span>
+        			</center>
+	        	</div>
+	        </div>
+	    </div>
+	</div>
+	<div class="row">
+    	<div class="col-xs-6">
+	      	<div class="box">
+	        	<div class="box-body">
+	        		<center>
+        			<a href="<?php bloginfo('template_url'); ?>/controller.php?ctr=relatorio&mt=relatorioMsgEnviada" title="Mensagem Enviada" ><i class="fa fa-bar-chart"></i></a><br>
+        			<span>Relatório de Mensagens Enviadas</span>
+        			</center>
+	        	</div>
+	        </div>
+	    </div>
+	    <div class="col-xs-6">
+	      	<div class="box">
+	        	<div class="box-body">
+	        		<center>
+        			<a href="<?php bloginfo('template_url'); ?>/controller.php?ctr=relatorio&mt=relatorioMsgRecebida" title="Mensagem Recebida" ><i class="fa fa-line-chart"></i></a><br>
+        			<span>Relatório de Mensagens Recebidas</span>
+        			</center>
+	        	</div>
+	        </div>
+	    </div>
+	</div>
+</section>
---
+++ instituicao/Instituicao.class.php
@@ -0,0 +1,138 @@
+<?php
+/**
+ * Classe para trabalhar os dados das instituicoes do sms social
+ */
+class Instituicao {
+
+	/**
+	 * Metodo construtor da class
+	 */ 
+	public function __construct() {}
+
+	/**
+	 * Metodo para inserir instituicao na base de dados tabela smssocial_instituicao
+	 * 
+	 * @Params
+	 * @$instituicoes: array com os dados do formulario
+	 */
+	public function insertInstituicao($instituicao) {
+
+		//variaveis globais
+		global $wpdb, $table_prefix;
+
+		//array para montar os dados novos
+		$inst = array();
+
+		//insert colunas e valores		
+		$inst["instituicao"] 	= $instituicao["instituicao"];
+		$inst["id_usuario_wp"] 	= $instituicao["id_usuario_wp"];
+		
+		//verifica se existe o registro
+		if($this->getRegistro($instituicao)) {
+			//executa a insercao
+			if($wpdb->insert("{$table_prefix}smssocial_instituicao", $inst)) {
+				
+				$_SESSION["msgOk"] = "Instituição incluida com sucesso!";
+
+				//return o valor inserido
+				return  $wpdb->insert_id;
+
+			} else {
+				
+				$_SESSION["msgErro"] = "Erro ao inserir um novo instituição!";
+
+				return false;
+			} // fim verificacao
+		}
+
+	} // fim insertInstituicao
+
+	/**
+	 * Metodo para alterar os dados na tabela smssocial_instituicao
+	 * 
+	 * @Params:
+	 * @instituicao: array com os dados para alteração na base de dados 
+	 */ 
+	public function updateInstituicao( $instituicao ) {
+
+		//variaveis globais
+		global $wpdb, $table_prefix;
+
+		//altera os valores para os valores passados		
+		$inst["instituicao"] 	= $instituicao["instituicao"];
+		$inst["id_usuario_wp"] 	= $instituicao["id_usuario_wp"];
+		$inst["dt_cadastro"] 	= date('Y-m-d H:i:s');
+
+		//verifica se existe o registro
+		if($this->getRegistro($instituicao)) {
+
+			//valor
+			$where  = array('id'=>$instituicao["id"]);
+
+			//executa a alteracao
+			if($wpdb->update("{$table_prefix}smssocial_instituicao", $inst, $where)) {
+				$_SESSION["msgOk"] = "Instituição alterada com sucesso!";
+				//retorna o valor do id do post
+				return $instituicao["id"];
+			} else {
+				$_SESSION["msgErro"] = "Erro ao alterar do instituição!";
+				//retorna como falso
+				return false;
+			} // fim verificacao
+		}
+
+	} // fim updateInstituicao
+
+	/**
+	 * Metodo para inativar o registro da instituicao
+	 * 
+	 * @Params:
+	 * @id: identificador da tabela para deixar o registro inativo
+	 * 
+	 */ 
+	public function deleteInstituicao($id) {
+
+		//variaveis globais
+		global $wpdb, $table_prefix;
+
+		//seta como inativo o valor
+		$inst["flg_atv"] = 0;
+
+		//valor
+		$where  = array('id'=>$id);
+
+		//executa a alteracao
+		if($wpdb->update("{$table_prefix}smssocial_instituicao", $inst, $where)) {
+			$_SESSION["msgOk"] = "Instituição deletado com sucesso!";
+		} else {
+			$_SESSION["msgErro"] = "Erro ao deletar o instituição!";
+		} // fim verificacao
+		
+	} // fim deleteInstituicao
+
+	/**
+	 * Metodo para verificar se existe o registro na tabela antes de gravar
+	 * 
+	 */
+	 private function getRegistro($instituicao) {
+	 	//variaveis globais
+		global $wpdb, $table_prefix;
+		
+	 	//verifica se já existe o grupo para aquela instituição
+		$vInstituicao = $wpdb->get_row("SELECT * 
+										FROM {$table_prefix}smssocial_instituicao
+										WHERE instituicao = '{$instituicao['instituicao']}'");
+
+		//verifica se o grupo existe
+		if(!empty($vInstituicao)) {
+			$_SESSION["msgErro"] = "Esta instituição já existe!";
+
+			return false;
+		}//fim vGrupo
+
+		return true;
+
+	 } //fim getRegistro
+
+} //fim class instituicao
+?>
